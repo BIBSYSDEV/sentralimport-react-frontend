8719b192597a7a3d3b3bb2ac2efd8a60af5b127b
Martin Kjærnet Bjerkeengen <martikbj@uio.no>
2019-07-24 12:32:02 +0200

    fikset react-hooks til modal

--- src/Components/ResultModal/ResultModal.js
+++ src/Components/ResultModal/ResultModal.js
@@ -9,64 +9,61 @@ import {
 import { fontWeight } from "@material-ui/system";
 import { RadioGroup, Radio } from "@material-ui/core";
 
-class ResultModal extends React.Component {
-  state = {
-    modal: false,
-    index: this.props.index,
-    selected: "0"
-  };
+export default function ResultModal(props) {
+  const [selected, setSelected] = React.useState("0");
 
-  render() {
-    return (
-      <Modal isOpen={this.props.open} className={this.props.className}>
-        <ModalHeader toggle={this.props.handleClose}>
-          Importvalg for resultat
-        </ModalHeader>
-        <ModalBody>
-          <ListGroup flush>
-            <ListGroupItem>
-              <ListGroupItemHeading>Importpublikasjon:</ListGroupItemHeading>
-              <ListGroupItemText>
-                {this.props.data.participants.map(participant => (
-                  <text>
-                    {participant.surname}, {participant.first_name};{" "}
-                  </text>
-                ))}
-                {this.props.data.title.en || this.props.data.title.nb}
-              </ListGroupItemText>
-            </ListGroupItem>
-            <ListGroupItem>
-              <ListGroupItemHeading>Cristinpublikasjoner:</ListGroupItemHeading>
-              <ListGroupItemText>
-                Det finnes ingen Cristinpublikasjoner som matcher
-                importpublikasjonen
-              </ListGroupItemText>
-            </ListGroupItem>
-            <ListGroupItem>
-              <div>
-                <Radio
-                  checked={this.state.selected === "0"}
-                  onChange={this.handleChange}
-                  value="0"
-                  label="Ikke importer"
-                />
-                <Radio
-                  checked={this.state.selected === "1"}
-                  onChange={this.handleChange}
-                  value="1"
-                />
-              </div>
-            </ListGroupItem>
-          </ListGroup>
-        </ModalBody>
-        <ModalFooter>
-          <Button color="secondary" onClick={this.props.handleClose}>
-            Lukk
-          </Button>
-        </ModalFooter>
-      </Modal>
-    );
+  function handleChange(event) {
+    setSelected(event.target.value);
   }
+
+  return (
+    <Modal isOpen={props.open}>
+      <ModalHeader toggle={props.handleClose}>
+        Importvalg for resultat
+      </ModalHeader>
+      <ModalBody>
+        <ListGroup flush>
+          <ListGroupItem>
+            <ListGroupItemHeading>Importpublikasjon:</ListGroupItemHeading>
+            <ListGroupItemText>
+              {props.data.participants.map(participant => (
+                <text>
+                  {participant.surname}, {participant.first_name};{" "}
+                </text>
+              ))}
+              {props.data.title.en || props.data.title.nb}
+            </ListGroupItemText>
+          </ListGroupItem>
+          <ListGroupItem>
+            <ListGroupItemHeading>Cristinpublikasjoner:</ListGroupItemHeading>
+            <ListGroupItemText>
+              Det finnes ingen Cristinpublikasjoner som matcher
+              importpublikasjonen
+            </ListGroupItemText>
+          </ListGroupItem>
+          <ListGroupItem>
+            <div>
+              <Radio
+                checked={selected === "0"}
+                onChange={handleChange}
+                value="0"
+              />
+              <Radio
+                checked={selected === "1"}
+                onChange={handleChange}
+                value="1"
+              />
+            </div>
+          </ListGroupItem>
+        </ListGroup>
+      </ModalBody>
+      <ModalFooter>
+        <Button color="secondary" onClick={props.handleClose}>
+          Lukk
+        </Button>
+      </ModalFooter>
+    </Modal>
+  );
 }
 
 ResultModal.defaultProps = {
@@ -76,7 +73,6 @@ ResultModal.defaultProps = {
       en: "The title"
     },
     participants: []
-  }
+  },
+  isOpen: false
 };
-
-export default ResultModal;
