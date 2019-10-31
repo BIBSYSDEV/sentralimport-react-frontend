1050993e37620872c8f2c438801330b19ed5228c
martikbj <martin.bjerkeengen@unit.no>
2019-10-31 15:40:05 +0100

    la til manglende polyfills for ie 11

---
+++ src/repeat.js
@@ -0,0 +1,6 @@
+// dependencies for å rendere app i Internet Explorer 11
+
+import "react-app-polyfill/ie11";
+import "core-js/features/string/repeat";
+import "core-js/features/symbol/iterator";
+import "core-js/features/array/find";
