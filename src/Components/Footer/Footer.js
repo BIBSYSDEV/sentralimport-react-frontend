15727394ffda22df231de14e51f7502a73df74b8
martikbj <martin.bjerkeengen@unit.no>
2020-03-10 10:50:42 +0100

    CRISIMP-230 added footer with logo and version number

---
+++ src/Components/Footer/Footer.js
@@ -0,0 +1,29 @@
+import React from "react";
+import { Navbar, Nav } from "react-bootstrap";
+import logo from "../Header/logo.svg";
+
+
+export default function Footer() {
+    const { version } = require('../../../package.json');
+
+    const footerStyle = {
+        marginTop: "10px"
+    }
+    
+    return (
+        <Navbar expand="sm" bg="light" variant="dark" style={footerStyle}>
+            <Nav>
+                <img
+                    src={logo}
+                    alt="CRISTIN - Current Research Information System In Norway"
+                />
+            </Nav>
+            <Navbar.Collapse>
+                <Nav className="mr-auto" />
+                <Nav>
+                    <Nav.Item className="footer-title"> Versjon: {version} </Nav.Item>
+                </Nav>
+            </Navbar.Collapse>
+        </Navbar>
+    )
+}
