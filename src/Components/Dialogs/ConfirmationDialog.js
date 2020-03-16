586a9b69621c2ff20cb63eaa8efd74fcea1288b4
martikbj <martin.bjerkeengen@unit.no>
2020-03-16 09:18:58 +0100

    cleanup

--- src/Components/Dialogs/ConfirmationDialog.js
+++ src/Components/Dialogs/ConfirmationDialog.js
@@ -64,7 +64,7 @@ export default function ConfirmationDialog(props) {
             }
             return {result: null, status: e.response !== undefined ? e.response.status : 500};
         }
-        let title = publication.title.length > 14 ? (publication.title.hasOwnProperty("en") ? publication.title.en.substr(0, 15) : publication.title.nb.substr(0, 15)) : publication.title.hasOwnProperty("en") ? publication.title.en : publication.title.nb;
+        let title = (publication.title.en.length > 14 || publication.title.nb.length > 14) ? (publication.title.hasOwnProperty("en") ? publication.title.en.substr(0, 15) : publication.title.nb.substr(0, 15)) : publication.title.hasOwnProperty("en") ? publication.title.en : publication.title.nb;
         return {result: {id: publication.pub_id, title: title}, status: 200};
     }
 
@@ -99,7 +99,6 @@ export default function ConfirmationDialog(props) {
         for (let i = 0; i < temp.publication.languages.length; i++) {
             title[temp.publication.languages[i].lang.toLowerCase()] = temp.publication.languages[i].title;
         }
-        console.log(title);
 
         let journal = {
             name: temp.publication.channel.title,
