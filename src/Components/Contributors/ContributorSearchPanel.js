a5217f83eabe04c99b7f6098ada5f92d1cc34b6c
martikbj <martin.bjerkeengen@unit.no>
2020-04-06 14:06:11 +0200

    CRISIMP-260 created list for displaying contributor suggestions

---
+++ src/Components/Contributors/ContributorSearchPanel.js
@@ -0,0 +1,30 @@
+import React from "react";
+import { Card, Button, Collapse } from "@material-ui/core"
+
+export default function ContributorSearchPanel(props) {
+
+    if(props.data !== ""){
+
+    return (
+        <Collapse in={props.collapsed}>
+            <Card className="contributor-suggestions">
+                <div>
+                    {props.data.map((author, i) => (
+                        <span key={i}> 
+                            <div className="author"> {author.first_name + " " + author.surname} </div>
+                            {author.affiliations.map((affiliation, h) => (
+                                <div key={h} className="affiliations"> {affiliation.institutionName} </div>
+                            ))}
+                            <Button key={i} onClick={() => props.handleChoose(author)} className="select-button"> Velg denne </Button>
+                            <hr />
+                        </span>
+                    ))}
+                </div>
+                <Button onClick={() => props.handleAbort()}> Lukk </Button>
+            </Card>
+        </Collapse>
+    );
+    } else {
+        return null;
+    }
+}
