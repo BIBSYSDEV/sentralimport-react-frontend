86d4b7779f146f8a6ff9d589dc5aa068f8e42762
pcb <pcb@unit.no>
2021-12-01 10:18:44 +0100

    SMILE-1174-formik_for_publication_compare

--- src/Components/ComparePublicationData/CompareFormCategory.tsx
+++ src/Components/ComparePublicationData/CompareFormCategory.tsx
@@ -1,6 +1,6 @@
-import { MenuItem, TextField, Typography } from '@material-ui/core';
+import { TextField, Typography } from '@material-ui/core';
 import ActionButtons from './ActionButtons';
-import { ErrorMessage, Field, FieldProps, useFormikContext } from 'formik';
+import { Field, FieldProps, useFormikContext } from 'formik';
 import React, { FC } from 'react';
 import {
   StyledLineCristinValue,
@@ -10,7 +10,7 @@ import {
 } from './CompareFormWrappers';
 import { CategoryOption, compareFormValuesType } from './ComparePublicationDataModal';
 import { ImportPublication } from '../../types/PublicationTypes';
-//import Select from 'react-select';
+import { Autocomplete } from '@material-ui/lab';
 
 interface CompareFormCategoryProps {
   importPublication: ImportPublication;
@@ -34,39 +34,26 @@ const CompareFormCategory: FC<CompareFormCategoryProps> = ({ importPublication, 
         }
       />
       <StyledLineCristinValue data-testid="cristindata-category-select">
-        <Field name="title">
-          {({ field, meta: { error } }: FieldProps) => (
-            <TextField
+        <Field name="category">
+          {({ field }: FieldProps) => (
+            <Autocomplete
+              //TODO: select isteden ?
+              fullWidth
               {...field}
               id="cristindata-category"
-              name="category"
-              placeholder="Kategori"
-              data-testid="cristindata-category-textfield"
-              inputProps={{ 'data-testid': 'cristindata-category-textfield-input' }}
-              fullWidth
-              select
-              variant="outlined"
-              value={field.value}
-              error={!!error}
-              helperText={<ErrorMessage name={field.name} />}>
-              {categories?.map((category: CategoryOption) => (
-                <MenuItem value={category.value}>{category.label}</MenuItem>
-              ))}
-            </TextField>
+              autoHighlight
+              noOptionsText="ingen kategorier funnet"
+              data-testid="cristindata-category-select"
+              options={categories ?? []}
+              getOptionLabel={(option) => option.label}
+              getOptionSelected={(option, value) => option.value === value.value}
+              onChange={(e, value: CategoryOption) => value && setFieldValue('category', value)}
+              renderInput={(params) => (
+                <TextField {...params} data-testid="cristindata-category-select-textfield" variant="outlined" />
+              )}
+            />
           )}
         </Field>
-
-        {/*//TODO: material /}
-        {/*<Select*/}
-        {/*  aria-label="Kategori"*/}
-        {/*  placeholder="Søk på kategori"*/}
-        {/*  name="categorySelect"*/}
-        {/*  options={categories}*/}
-        {/*  value={selectedCategory}*/}
-        {/*  className="basic-select"*/}
-        {/*  classNamePrefix="select"*/}
-        {/*  onChange={handleChangeCategory}*/}
-        {/*/>*/}
       </StyledLineCristinValue>
     </StyledLineWrapper>
   );
