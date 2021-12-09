175ff6ddf8ef1f1adf23ddd7f29c1e4360141558
pcb <pcb@unit.no>
2021-12-09 13:34:16 +0100

    bugfix/fix-tests

--- cypress/integration/comparePublicationDataModal.spec.js
+++ cypress/integration/comparePublicationDataModal.spec.js
@@ -7,19 +7,28 @@ context('importModal', () => {
     cy.visit('/');
   });
 
-  it('can validate new journal registration', () => {
-    const mockInvalidIssn = '123412341234';
-    cy.get(`[data-testid="import-table-row-${mockImportData[0].pubId}"]`).click();
-    cy.get(`[data-testid="duplication-modal-ok-button"]`).click();
-    cy.get(`#create-journal-header`).click();
+  it(
+    'can validate new journal registration',
+    {
+      retries: {
+        runMode: 2,
+        openMode: 1,
+      },
+    },
+    () => {
+      const mockInvalidIssn = '123412341234';
+      cy.get(`[data-testid="import-table-row-${mockImportData[0].pubId}"]`).click();
+      cy.get(`[data-testid="duplication-modal-ok-button"]`).click();
+      cy.get(`#create-journal-header`).click();
 
-    cy.get(`[data-testid="new-journal-form-issn-input"]`).type(mockInvalidIssn);
-    cy.get(`[data-testid="submit-create-journal-button"]`).click();
-    //
-    // cy.waitFor(`#new-journal-title-helper-text`);
-    cy.get(`#new-journal-title-helper-text`).contains('Tittel er et obligatorisk felt');
-    cy.get(`#new-journal-issn-helper-text`).contains('SSN er ikke på korrekt format (NNNN-NNNC)');
-    cy.get(`[data-testid="new-journal-form-error"]`).contains('Det er feil i tidsskrift-skjema');
-    cy.get(`[data-testid="submit-create-journal-button"]`).should('exist');
-  });
+      cy.get(`[data-testid="new-journal-form-issn-input"]`).type(mockInvalidIssn);
+      cy.get(`[data-testid="submit-create-journal-button"]`).click();
+      //
+      // cy.waitFor(`#new-journal-title-helper-text`);
+      cy.get(`#new-journal-title-helper-text`).contains('Tittel er et obligatorisk felt');
+      cy.get(`#new-journal-issn-helper-text`).contains('SSN er ikke på korrekt format (NNNN-NNNC)');
+      cy.get(`[data-testid="new-journal-form-error"]`).contains('Det er feil i tidsskrift-skjema');
+      cy.get(`[data-testid="submit-create-journal-button"]`).should('exist');
+    }
+  );
 });
