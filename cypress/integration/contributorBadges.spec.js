fa117264304a0b2261d88de3e8f0a1a2ee077087
anetteOlli <ansi@unit.no>
2021-12-15 14:28:46 +0100

    SMILE-1189-badge-contributorform, added tests

--- cypress/integration/contributorBadges.spec.js
+++ cypress/integration/contributorBadges.spec.js
@@ -8,7 +8,6 @@ context('contributor badges', () => {
     cy.visit('/');
   });
 
-  /*
   it('shows bagdes on verified cristin person from importdata', () => {
     cy.get(`[data-testid="import-table-row-${mockImportData[1].pubId}"]`).click();
     cy.get('[data-testid="duplication-modal-ok-button"]').click();
@@ -18,15 +17,11 @@ context('contributor badges', () => {
     cy.get('[data-testid="contributor-form-2-name"]').should('have.css', 'color', Colors.Text.OPAQUE_54_BLACK);
   });
 
-   */
-
   it('retains contributor badges from the search panel', () => {
     cy.get(`[data-testid="import-table-row-${mockImportData[1].pubId}"]`).click();
     cy.get('[data-testid="duplication-modal-ok-button"]').click();
     cy.get('[data-testid="open-contributors-modal-button"]').click();
-    cy.wait(500);
     cy.get('[data-testid="contributor-search-button-2"]').click();
-    cy.wait(500);
 
     //choose unknown cristin author:
     cy.get(`[data-testid="add-only-person-${mockForbiddenPerson.cristin_person_id}"]`).click();
@@ -46,7 +41,6 @@ context('contributor badges', () => {
 
     //Choose no cristin author:
     cy.get('[data-testid="contributor-search-button-2"]').click();
-    cy.wait(500);
     cy.get('[data-testid="choose-text-field-person-2"]').click();
     cy.get(
       `[data-testid="not-verified-contributor-badge-${mockPersonWithoutAffiliationAttribute.cristin_person_id}"]`
