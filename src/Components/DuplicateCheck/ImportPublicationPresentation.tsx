0c36f22e7ff5afb4ad5be99a4e48c2d99cb4c766
pcb <pcb@unit.no>
2022-01-05 14:11:04 +0100

    SMILE-1155

--- src/Components/DuplicateCheck/ImportPublicationPresentation.tsx
+++ src/Components/DuplicateCheck/ImportPublicationPresentation.tsx
@@ -1,16 +1,34 @@
 import React, { FC } from 'react';
-import { ListGroupItemHeading } from 'reactstrap';
 import { Markup } from 'interweave';
 import { cleanTitleForMarkup } from '../../utils/stringUtils';
 import styled from 'styled-components';
 import { Typography } from '@material-ui/core';
 import { ImportPublication } from '../../types/PublicationTypes';
+import { Colors } from '../../assets/styles/StyleConstants';
 
-const StyledAuthorTypography = styled(Typography)`
+const StyledImportPublicationPresentation = styled.div`
+  padding-left: 1rem;
+  margin: 1rem 0 1rem 0.4rem;
+  border-left: 0.4rem solid ${Colors.PURPLE};
+  color: ${Colors.PURPLE};
+  word-wrap: break-word;
+`;
+
+const StyledTitleTypography = styled(Typography)`
   && {
+    margin: 0 0 0.5rem;
     font-weight: bold;
+    line-height: 1.2;
   }
 `;
+
+const generateAuthorPresentation = (importPublication: ImportPublication) => {
+  return importPublication.authors
+    .slice(0, 3)
+    .map((author: any) => author.authorName)
+    .join('; ')
+    .concat(importPublication.authors.length > 3 ? ' et al.' : '');
+};
 
 interface ImportPublicationPresentationProps {
   importPublication: ImportPublication;
@@ -19,22 +37,17 @@ interface ImportPublicationPresentationProps {
 const ImportPublicationPresentation: FC<ImportPublicationPresentationProps> = ({ importPublication }) => {
   return (
     <>
-      <ListGroupItemHeading>Importpublikasjon:</ListGroupItemHeading>
-      <div data-testid="duplicate-check-importdata">
-        {importPublication.authors.slice(0, 5).map((author) => (
-          <StyledAuthorTypography display="inline" key={author.sequenceNr}>
-            {author.authorName};{' '}
-          </StyledAuthorTypography>
-        ))}
-        {importPublication.authors.length > 5 && 'et al (' + importPublication.authors.length + ') '}
-        {importPublication?.languages && <Markup content={cleanTitleForMarkup(importPublication.languages[0].title)} />}
-        <i>{importPublication.channel && ' ' + importPublication.channel.title + ' '}</i>
-        {importPublication.yearPublished + ';'}
-        {importPublication.channel && importPublication.channel.volume + ';'}
-        {importPublication.channel && importPublication.channel.pageFrom && importPublication.channel.pageFrom + '-'}
-        {importPublication.channel && importPublication.channel.pageTo && importPublication.channel.pageTo}
-        {importPublication.doi && ' doi:' + importPublication.doi}
-      </div>
+      <Typography variant="h6">Importpublikasjon:</Typography>
+      <StyledImportPublicationPresentation data-testid="duplicate-check-importdata">
+        <StyledTitleTypography>
+          {importPublication?.languages && (
+            <Markup content={cleanTitleForMarkup(importPublication.languages[0].title)} />
+          )}
+        </StyledTitleTypography>
+        <Typography>{generateAuthorPresentation(importPublication)}</Typography>
+        <Typography>{importPublication.channel?.title}</Typography>
+        <Typography>{importPublication.yearPublished}</Typography>
+      </StyledImportPublicationPresentation>
     </>
   );
 };
