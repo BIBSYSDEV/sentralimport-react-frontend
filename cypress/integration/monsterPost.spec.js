c025b8cc6739cdec256d2edc7425848cb530d408
pcb <pcb@unit.no>
2022-03-28 15:49:22 +0200

    SMILE-1468-remove-cristinid_on_unidentified_persons

--- cypress/integration/monsterPost.spec.js
+++ cypress/integration/monsterPost.spec.js
@@ -1,7 +1,7 @@
 import { mockDoiForMonsterPublication } from '../../src/utils/mockdata';
 import mockMonsterImportPost from '../../src/utils/mockMonsterImportPost.json';
 
-context('application', () => {
+context('monsterpost', () => {
   beforeEach(() => {
     cy.login();
     cy.visit('/');
@@ -47,7 +47,7 @@ context('application', () => {
     cy.get('[data-testid="importdata-author-presentation-610220"]').click();
     cy.get(`[data-testid="duplication-modal-ok-button"]`).click();
 
-    cy.get(`[data-testid="contributor-errors"]`).contains('11, 12 (Duplisert bidragsyter med cristinId: 234)');
+    cy.get(`[data-testid="contributor-errors"]`).contains('11, 12 (Duplisert bidragsyter med cristinId: 2345)');
     cy.get(`[data-testid="contributor-errors"]`).contains('13, 14 (Duplisert bidragsyter med cristinId: 666)');
 
     cy.get(`[data-testid="open-contributors-modal-button"]`).click();
